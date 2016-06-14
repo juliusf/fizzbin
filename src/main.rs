e27239aa770537d5234b242ebdb94a5f789c9396
Julius F <julius.flohr@gmail.com>
2016-06-14 20:34:41 +0200

    added roms. added binary reader.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::io::Read;
+use std::path::Path;
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    let rom_path = env::args().nth(1).unwrap();
+    let rom = read_binary(&rom_path);
+
+}
+
+fn read_binary<P: AsRef<Path>>(path: P) -> Box<[u8]>{
+    let mut file = File::open(path).unwrap();
+    let mut file_buf = Vec::new();
+    file.read_to_end(&mut file_buf).unwrap();
+    file_buf.into_boxed_slice()
 }
